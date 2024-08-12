29d6c95507eb0ca2d2f106b649824d65f3675c78
XEun-0 <buttereflay@gmail.com>
2024-08-12 02:14:27 -0700

    testing code

--- Devices/Arduino/Blink/Blink.ino
+++ Devices/Arduino/Blink/Blink.ino
@@ -1,8 +1,13 @@
 void setup() {
+  Serial.begin(115200);
+  
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  if (Serial.available())
+    Serial.write( toupper (Serial.read()) );
+  
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
