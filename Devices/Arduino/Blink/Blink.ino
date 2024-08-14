8204d691d402050772e36b9164b3578e8f3359bc
xenophed <buttereflay@gmail.com>
2024-08-14 02:46:55 -0700

    testing serial succ

--- Devices/Arduino/Blink/Blink.ino
+++ Devices/Arduino/Blink/Blink.ino
@@ -1,5 +1,5 @@
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   
   pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -7,9 +7,4 @@ void setup() {
 void loop() {
   if (Serial.available())
     Serial.write( toupper (Serial.read()) );
-  
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
 }
