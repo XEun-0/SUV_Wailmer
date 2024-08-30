afac3b5acc76ecc1b93e852ef07187ca5f194a3b
xenophed <buttereflay@gmail.com>
2024-08-30 07:24:22 -0700

    update ros

--- Devices/Arduino/Libraries/lib_install.sh
+++ Devices/Arduino/Libraries/lib_install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 # Absolute path to the library directory
-LIBRARY_PATH="~/../workspace/ROS2-SW/Devices/Arduino/Libraries/Adafruit_BNO055"
+#LIBRARY_PATH="Adafruit_Unified_Sensor-1.1.14.zip \
+#              Adafruit_BNO055-1.6.3.zip \
+#              BlueRobotics_MS5837_Library-1.1.0.zip \
+#              Adafruit_BusIO-1.16.1.zip"
+
+# arduino-cli lib install Adafruit_BNO055-1.6.3.zip 
+# arduino-cli config set library.enable_unsafe_install true
+# arduino-cli config dump
+# arduino-cli lib list
 
 # Install the library
-arduino-cli lib install "$LIBRARY_PATH"
-
+arduino-cli lib install --zip-path Adafruit_Unified_Sensor-1.1.14.zip
+arduino-cli lib install --zip-path Adafruit_BNO055-1.6.3.zip
+arduino-cli lib install --zip-path Adafruit_BusIO-1.16.1.zip
+arduino-cli lib install --zip-path BlueRobotics_MS5837_Library-1.1.0.zip
