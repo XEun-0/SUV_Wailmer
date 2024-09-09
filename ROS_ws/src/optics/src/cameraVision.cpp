a244561a0f565b817dc4f10caa4676849638e189
XEun-0 <buttereflay@gmail.com>
2024-09-09 14:07:25 -0700

    optics and operations packages

---
+++ ROS_ws/src/optics/src/cameraVision.cpp
@@ -0,0 +1,11 @@
+#include<chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+using namespace std::chrono_literals;
+
+class CameraVision : public rclcpp::Node {
+	public:
+	private:
+}
